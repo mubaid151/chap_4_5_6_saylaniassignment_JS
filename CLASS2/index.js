121968ebedc9394f0ffeb704c31355c6e7bc460c
Mohammad Ubaid <ubaidmohammad151@gmail.com>
2024-08-19 12:34:04 +0500

    Add files via upload

---
+++ CLASS2/index.js
@@ -0,0 +1,119 @@
+// CHAPTER NO 4 VARIABLE NAME: LEGAL AND IILEGAL
+// Declare 3 variables in one statement
+
+var a=1, b=2, c=3;
+
+// Declare 5 legal and illegal variable names
+// legal varaible names
+ var myVariable = "ubaid";
+var _underscore = "123";
+ var $dollarSignVar = "score";
+var camelCase = "camel";
+var variable123 = "variable";
+
+// illegal variable names
+
+var 123variable = "123";
+var my-variable = "variable";
+var var = "ubaid";
+var my variable = "char"; 
+var @specialchar = "special";
+
+// CHAPTER NO 5 MATH EXPRESSIONS
+
+var a = 8;
+var b = 8;
+var sum = a+b;
+var multiply = a*b;
+var division = a/b;
+var Modulus = a%b;
+
+console.log("sum is: " +sum);
+console.log("Multiplication is: " +multiply);
+console.log("Division is: " +division);
+console.log("moduslus is: " +Modulus);
+
+// Do the following mathematical expression in JS:
+// a)
+ var u;
+//  c)
+var u = 10;
+// e)
+u++;
+// g)
+
+u += 7;
+
+// i)
+u--;
+// k)
+var Modulus = a%b;
+
+// COST OF ONE MOVIES IS 600PKR
+var movies = 600*5;
+
+console.log("TOTAL COST TO BUY FIVE TICKETS TO A MOVIE IS: " +movies);
+
+// Table of 4
+console.log("4 x 1 = " + (4 * 1));
+console.log("4 x 2 = " + (4 * 2));
+console.log("4 x 3 = " + (4 * 3));
+console.log("4 x 4 = " + (4 * 4));
+console.log("4 x 5 = " + (4 * 5));
+console.log("4 x 6 = " + (4 * 6));
+console.log("4 x 7 = " + (4 * 7));
+console.log("4 x 8 = " + (4 * 8));
+console.log("4 x 9 = " + (4 * 9));
+console.log("4 x 10 = " + (4 * 10));
+
+
+var celsius = 30;  // example value in Celsius
+var fahrenheit = (celsius * 9/5) + 32;
+console.log(celsius + "°C is equal to " + fahrenheit + "°F");
+
+var fahrenheitValue = 86;  // example value in Fahrenheit
+var celsiusValue = (fahrenheitValue - 32) * 5/9;
+console.log(fahrenheitValue + "°F is equal to " + celsiusValue + "°C");
+
+// 7)write a programe to checkout process of a shopping cart system of an e commerce website store the following in variables 
+
+var item1Name = "Laptop";
+var item1Price = 800;  
+var item1Quantity = 1;
+
+var item2Name = "Headphones";
+var item2Price = 100;
+var item2Quantity = 2;
+
+var item3Name = "Mouse";
+var item3Price = 20;
+var item3Quantity = 3;
+
+// Chapter 6 Math expression
+
+var abc = 10;
+
+++abc;
+console.log(abc);
+
+abc++;
+console.log(abc);
+
+--abc;
+console.log(abc);
+
+abc--;
+console.log(abc);
+
+var b=2, a=1;
+var result = --a - --b + ++b + b--;
+
+console.log(result);
+
+
+
+
+
+
+
+
